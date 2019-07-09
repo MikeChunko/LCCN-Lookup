2f7bcd463b3d6b8038444658fad19eec27cae6d4
Mike Chunko <chunkomike@gmail.com>
2019-07-09 12:08:15 -0400

    Given a search link, the program will find and print out the HTML line corresponding to the title, author, and LCCN of every search result

--- LCCN.java
+++ LCCN.java
@@ -9,7 +9,29 @@ public class LCCN {
         BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
         String inputLine;
         while ((inputLine = in.readLine()) != null) {
-            System.out.println(inputLine);
+            if (inputLine.contains("bibId") && !inputLine.contains("Links available")) {
+                String bibId = inputLine.substring(inputLine.indexOf("bibId=") + 6, inputLine.indexOf("\">"));
+
+                URL bib_url = new URL("https://catalog.loc.gov/vwebv/holdingsInfo?bibId=" + bibId);
+                BufferedReader bib_in = new BufferedReader(new InputStreamReader(bib_url.openStream()));
+                String bib_inputLine;
+                while ((bib_inputLine = bib_in.readLine()) != null) {
+                    if (bib_inputLine.contains("Personal name")) {
+                        bib_inputLine = bib_in.readLine();
+                        bib_inputLine = bib_in.readLine();
+                        bib_inputLine = bib_in.readLine();
+                        System.out.println("Author:" + bib_inputLine.trim());
+                    }
+                    if (bib_inputLine.contains("CALL NUMBER</h3>")) {
+                        bib_inputLine = bib_in.readLine();
+                        bib_inputLine = bib_in.readLine();
+                        bib_inputLine = bib_in.readLine();
+                        System.out.println("LCCN:" + bib_inputLine.trim());
+                    }
+                    if (bib_inputLine.contains("small class"))
+                        System.out.println("Title:" + bib_inputLine.trim());
+                }
+            }
         }
     }
 }

25c0299fe7b466c83fa9490a77b35626159daafc
Mike Chunko <chunkomike@gmail.com>
2019-07-11 11:21:12 -0400

    Prints out *only* the relevant data now (LCCN, author, title)

--- LCCN.java
+++ LCCN.java
@@ -22,23 +22,44 @@ public class LCCN {
                 BufferedReader bib_in = new BufferedReader(new InputStreamReader(bib_url.openStream()));
                 String bib_inputLine;
 
+                String call_number = "", author = "", title = "";
+
                 // Find the author, LCCN, and title of each search result
                 while ((bib_inputLine = bib_in.readLine()) != null) {
                     if (bib_inputLine.contains("Personal name")) {
                         bib_in.readLine(); // Skip 3 lines from the identifying HTML element
                         bib_in.readLine();
                         bib_inputLine = bib_in.readLine();
-                        System.out.println("Author:" + bib_inputLine.trim());
+                        author = bib_inputLine.trim();
                     }
-                    if (bib_inputLine.contains("CALL NUMBER</h3>")) {
+                    if (bib_inputLine.contains("LC classification</h3>")) {
                         bib_in.readLine(); // Skip 3 lines from the identifying HTML element
                         bib_in.readLine();
                         bib_inputLine = bib_in.readLine();
-                        System.out.println("LCCN:" + bib_inputLine.trim());
+
+                        call_number = bib_inputLine.trim();
                     }
                     if (bib_inputLine.contains("small class"))
-                        System.out.println("Title:" + bib_inputLine.trim());
+                        title = bib_inputLine.trim();
                 }
+
+                // Trim call_number, author, and title to be only the relevant data
+
+                // Trimming is not necessary in the case of no call number
+                if (call_number.equals(""))
+                    call_number = "Not Available";
+                else
+                    call_number = call_number.substring(call_number.indexOf("<span>") + 17, call_number.indexOf("</span>"));
+
+                // Trimming is not necessary in the case of no author
+                if (author.equals(""))
+                    author = "None Listed";
+                else
+                    author = author.substring(author.indexOf("<span") + 16, author.indexOf("</span>"));
+
+                title = title.substring(title.indexOf("</small>") + 8, title.indexOf("</h1>"));
+
+                System.out.println("lccn:[" + call_number + "], author:[" + author + "], title:[" + title + "]");
             }
         }
     }

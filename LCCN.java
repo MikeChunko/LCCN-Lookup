8ad8302bf9643caa26239bcc21af8dab343ce82a
Mike Chunko <chunkomike@gmail.com>
2019-07-13 20:19:55 -0400

    Can now read from a text file using -input_file="file name"

--- LCCN.java
+++ LCCN.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 
 /**
@@ -10,16 +8,44 @@ import java.net.URL;
 public class LCCN {
     public static void main(String[] args) throws IOException {
         String search_query;
+        boolean output_to_file = false;
+        String output_file = "";
 
-        // No arguments were given
         try {
-            search_query = args[0];
+            // An input file is specified
+            if (args[0].contains("-input_file")) {
+                String input_file = args[0].substring(args[0].indexOf("="));
+                BufferedReader br = new BufferedReader(new FileReader(new File(input_file)));
+                while ((search_query = br.readLine()) != null) {
+                    System.out.println("Results for '" + search_query + "'");
+                    search(search_query, output_to_file, output_file);
+                    System.out.println("---------------------------------");
+                }
+            }
+            else { // A single search query is given
+                search_query = args[0];
+                search(search_query, output_to_file, output_file);
+            }
         } catch (IndexOutOfBoundsException e) {
             throw new IllegalArgumentException("Please provide the search query as an argument");
         }
 
+
+        System.out.println("Program end");
+    }
+
+    /**
+     * Does all of the actual searching
+     *
+     * @param search_query   The term to be searched
+     * @param output_to_file Set to true if it should output to a specific file, false to output to stdout
+     * @param output_file    The output file, if one is specified
+     * @throws IOException
+     */
+    private static void search(String search_query, boolean output_to_file, String output_file) throws IOException {
         // The URL to be searched
         search_query = search_query.replaceAll(" ", "+");
+        search_query = search_query.replaceAll("\"", "");
         URL url = new URL("https://catalog.loc.gov/vwebv/search?searchCode=TALL&searchArg=" + search_query + "&searchType=1");
         BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
         String inputLine;
@@ -124,6 +150,5 @@ public class LCCN {
 
             System.out.println("lccn:[" + call_number + "], author:[" + author + "], title:[" + title + "]");
         }
-        System.out.println("program end");
     }
 }
